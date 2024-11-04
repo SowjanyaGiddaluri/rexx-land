84be0b472a133585bf2b0beb90f5ec8784cbc956
SowjanyaGiddaluri <sowjanyaworkspace1@gmail.com>
2024-11-04 18:13:44 +0530

    this is my project

---
+++ Assets/JavaScript/index.js
@@ -0,0 +1,50 @@
+/*swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,  // Autoplay continues even after user interaction
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});*/
+
+// tabbed content
+$(".tab_content").hide();
+$(".tab_content:first").show();
+
+/* if in tab mode */
+$("ul.tabs li").click(function() {
+    $(".tab_content").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+
+    $("ul.tabs li").removeClass("active");
+    $(this).addClass("active");
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(".tab_drawer_heading[rel^='" + activeTab + "']").addClass("d_active");
+});
+
+/* if in drawer mode */
+$(".tab_drawer_heading").click(function() {
+    $(".tab_content").hide();
+    var d_activeTab = $(this).attr("rel");
+    $("#" + d_activeTab).fadeIn();
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(this).addClass("d_active");
+
+    $("ul.tabs li").removeClass("active");
+    $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
+});
+
+/* Extra class "tab_last" 
+   to add border to right side
+   of last tab */
+$('ul.tabs li').last().addClass("tab_last");
